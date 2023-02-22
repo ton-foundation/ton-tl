7e04a2adcbedc3788dbc2e9df0cb736cb86fcd33
Dan Volkov <hi@dvlkv.com>
2023-02-23 02:06:48 +0400

    chore: update license

--- src/generator/generate.ts
+++ src/generator/generate.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Whales Corp. 
+ * All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { CombinatorDeclaration, ETypeIdentifier, Expression, OptionalVariableIdentifier } from "../parser/ast";
 import { parseSchema } from "../parser/parseSchema";
 import { CodeBuilder } from "./CodeBuilder";
